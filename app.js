2f90b9dd32cbf8600b172a1fe32d82812997dc42
noumansharif27 <noumansharifgul27.com>
2025-09-29 19:03:27 +0500

    adding functionality to the web

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const folder = document.querySelector(".folder");
 
 folder.addEventListener("mouseover", () => {
-  console.log("HI");
+  const image0 = document.querySelector(".image-0");
+  const image1 = document.querySelector(".image-1");
+  const image2 = document.querySelector(".image-2");
+
+  const randomNum = Math.floor(Math.random() * 5 + 1);
+  console.log(randomNum);
+
+  image0 = style.top = "0%";
+  image1 = style.top = "0%";
+  image2 = style.top = "-19%";
 });
