a6fb4bac4aadab00df3385e31b8993465966a82c
noumansharif27 <noumansharifgul27.com>
2025-09-30 11:59:08 +0500

    getting stacking z-idex error on images

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-const folder = document.querySelector(".folder");
+const folders = document.querySelectorAll(".folder");
 
-folder.addEventListener("mouseover", () => {
-  gsap.to(".image-0", {
-    y: "-48%",
-    ease: "easeOut",
-  });
+const folder = document.querySelectorAll(".folder");
 
-  gsap.to(".image-1", {
-    y: "-40%",
-    ease: "easeOut",
-  });
-  gsap.to(".image-2", {
-    y: "-42%",
-    ease: "easeOut",
+folder.forEach((element, index) => {
+  element.addEventListener("mouseover", () => {
+    console.log(`Hovered element index: ${index}`);
+    // You can also do your GSAP animation here
   });
 });
 
-folder.addEventListener("mouseout", () => {
-  gsap.to(".image-0", {
-    y: "-10%",
-    ease: "easeInOut",
+folders.forEach((folder) => {
+  const folderBottom = folder.querySelector(".folder-bottom");
+  folderBottom.addEventListener("mouseover", () => {
+    const image0 = folder.querySelector(".image-0");
+    const image1 = folder.querySelector(".image-1");
+    const image2 = folder.querySelector(".image-2");
+
+    gsap.to(image0, {
+      y: "-120%",
+      ease: "power4.out",
+      duration: 0.5,
+      rotate: "-10deg",
+    });
+
+    gsap.to(image1, {
+      y: "-110%",
+      ease: "power4.out",
+      duration: 0.5,
+      rotate: "10deg",
+    });
+    gsap.to(image2, {
+      y: "-120%",
+      ease: "power4.out",
+      duration: 0.5,
+      rotate: "0deg",
+    });
   });
 
-  gsap.to(".image-1", {
-    y: "-10%",
-    ease: "easeInOut",
-  });
-  gsap.to(".image-2", {
-    y: "-10%",
-    ease: "easeInOut",
+  folder.addEventListener("mouseout", () => {
+    gsap.to(".image-0", {
+      y: "10%",
+      ease: "easeInOut",
+      rotate: "0deg",
+    });
+
+    gsap.to(".image-1", {
+      y: "10%",
+      ease: "easeInOut",
+      rotate: "0deg",
+    });
+    gsap.to(".image-2", {
+      y: "10%",
+      ease: "easeInOut",
+      rotate: "0deg",
+    });
   });
 });
